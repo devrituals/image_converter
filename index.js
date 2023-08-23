80b6643775892ab6c2ccc3177223896de69ebcf0
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-23 01:30:23 +0100

    Update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+window.jsPDF = window.jspdf.jsPDF;
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("convertButton").addEventListener("click", function() {
         const imageInput = document.getElementById("imageInput");
