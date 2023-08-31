b3fa9fab7f5fff259147924bc2b1381cff0e9993
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-31 22:29:45 +0100

    Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     const imgData = e.target.result;
 
                     // Set PDF page size and orientation
-                    pdf.addImage(imgData, "JPEG", 20, 20, 168, 1123); // Adjust image placement and dimensions
+                    pdf.addImage(imgData, "JPEG", 20, 20, 168, 468); // Adjust image placement and dimensions
 
                     // Save the PDF
                     const blob = pdf.output("blob");
