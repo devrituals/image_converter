726a383ec1573dce782085482462f2e008e6e577
devrituals <abbelabbel9@gmail.com>
2024-04-17 01:49:15 +0100

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
           const imgData = e.target.result;
 
           // Increase the image dimensions for better quality
-		const maxWidth = 350; // Adjust this value as needed
+		const maxWidth = 280; // Adjust this value as needed
             const maxHeight = 150; // Adjust this value as needed
             const imgWidth = Math.min(this.width, maxWidth);
             const imgHeight = (this.height * imgWidth) / this.width;
