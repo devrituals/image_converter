e3d8019104f2fc1e01846d3de022c5fbd86b0083
devrituals <abbelabbel9@gmail.com>
2024-04-17 01:11:11 +0100

    update

--- index.js
+++ index.js
@@ -14,28 +14,20 @@ document.addEventListener("DOMContentLoaded", function() {
           // Convert to PDF format
           const pdf = new jsPDF();
           const imgData = e.target.result;
-          
-          // Set the compression level (0 = no compression, 1 = maximum compression)
-          const compressionLevel = 0.8; // Adjust this value between 0 and 1
 
-          // Get the image dimensions
-          const img = new Image();
-          img.src = imgData;
-          img.onload = function() {
-            // Calculate the image dimensions based on the aspect ratio
-            const imgWidth = 160;
-            const imgHeight = this.height * (imgWidth / this.width);
+          // Increase the image dimensions for better quality
+          const imgWidth = 300; // Adjust this value as needed
+          const imgHeight = 225; // Adjust this value as needed
 
-            // Add the image to the PDF with the desired compression level and dimensions
-            pdf.addImage(imgData, "JPEG", compressionLevel, 20, 20, imgWidth, imgHeight);
+          // Add the image to the PDF with the desired dimensions
+          pdf.addImage(imgData, "JPEG", 20, 20, imgWidth, imgHeight);
 
-            // Save the PDF
-            const blob = pdf.output("blob");
-            const pdfDataUrl = URL.createObjectURL(blob);
-            downloadLink.href = pdfDataUrl;
-            downloadLink.download = convertedFileName;
-            downloadLink.style.display = "flex";
-          };
+          // Save the PDF
+          const blob = pdf.output("blob");
+          const pdfDataUrl = URL.createObjectURL(blob);
+          downloadLink.href = pdfDataUrl;
+          downloadLink.download = convertedFileName;
+          downloadLink.style.display = "flex";
         } else {
           // Convert to image format
           const convertedDataUrl = e.target.result;
