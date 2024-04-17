64db09008b8bc84cbf9598881f9b362fe426c33c
devrituals <abbelabbel9@gmail.com>
2024-04-17 01:30:53 +0100

    update

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ document.addEventListener("DOMContentLoaded", function() {
             const imgHeight = (this.height * imgWidth) / this.width;
 
           // Add the image to the PDF with the desired dimensions
-          pdf.addImage(imgData, "JPEG", 20, 20, imgWidth, imgHeight);
+		const pageWidth = pdf.internal.pageSize.getWidth();
+const pageHeight = pdf.internal.pageSize.getHeight();
+const x = (pageWidth - imgWidth) / 2;
+const y = (pageHeight - imgHeight) / 2;
+
+// Add the image to the PDF with the calculated dimensions and centered coordinates
+pdf.addImage(imgData, "JPEG", x, y, imgWidth, imgHeight);
 
           // Save the PDF
           const blob = pdf.output("blob");
