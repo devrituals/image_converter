e8ffa783bdb5cd23a48a598d0141f572ef780630
devrituals <abbelabbel9@gmail.com>
2024-04-18 13:21:21 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.jsPDF = window.jspdf.jsPDF;
+window.jsPDF = window.jsPDF = window.jspdf.jsPDF;
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("convertButton").addEventListener("click", function() {
     const imageInput = document.getElementById("imageInput");
@@ -14,22 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
           // Convert to PDF format
           const pdf = new jsPDF();
           const imgData = e.target.result;
+          
+          // Set the compression level (0 = no compression, 1 = maximum compression)
+          const compressionLevel = 0.8; // Adjust this value between 0 and 1
 
-          // Increase the image dimensions for better quality
-		const maxWidth = 3508; // Adjust this value as needed
-            const maxHeight = 2479; // Adjust this value as needed
-            const imgWidth = Math.min(this.width, maxWidth);
-            const imgHeight = (this.height * imgWidth) / this.width;
+          // Get the image dimensions
+          const img = new Image();
+          img.src = imgData;
+          img.onload = function() {
+            // Calculate the image dimensions based on the aspect ratio
+            const imgWidth = 3508;
+            const imgHeight = this.height * (imgWidth / this.width);
 
-		// Add the image to the PDF with the calculated dimensions and centered coordinates
-		pdf.addImage(imgData, "JPEG", 3508, 2479, imgWidth, imgHeight);
+            // Add the image to the PDF with the desired compression level and dimensions
+            pdf.addImage(imgData, "JPEG", compressionLevel, 20, 20, imgWidth, imgHeight);
 
-          // Save the PDF
-          const blob = pdf.output("blob");
-          const pdfDataUrl = URL.createObjectURL(blob);
-          downloadLink.href = pdfDataUrl;
-          downloadLink.download = convertedFileName;
-          downloadLink.style.display = "flex";
+            // Save the PDF
+            const blob = pdf.output("blob");
+            const pdfDataUrl = URL.createObjectURL(blob);
+            downloadLink.href = pdfDataUrl;
+            downloadLink.download = convertedFileName;
+            downloadLink.style.display = "flex";
+          };
         } else {
           // Convert to image format
           const convertedDataUrl = e.target.result;
